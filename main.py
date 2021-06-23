ec583cf53520d7d454f8861209dd21ebd55c024a
Maingi112 <maingip42@gmail.com>
2021-06-23 03:41:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+color = input("Enter color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Lilies are " + color)
+print(plural_noun + " are pink")
+print("I love " + celebrity)
